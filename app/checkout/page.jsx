6fb44947f8847e6af7f87ca3f37b8ed3c9f2eb1b
Jihad-24 <mdjihat0021@gmail.com>
2026-05-18 17:04:36 +0600

    style: format destructuring for better readability in CheckoutPage

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -11,7 +11,8 @@ import axios from "axios";
 
 export default function CheckoutPage() {
   const router = useRouter();
-  const { items, hydrated, cartTotal, clearCart, delivery,setDelivery } = useCart();
+  const { items, hydrated, cartTotal, clearCart, delivery, setDelivery } =
+    useCart();
   const [submitting, setSubmitting] = useState(false);
   const { user } = useAuth();
   // const [delivery, setDelivery] = useState(70); // default inside Dhaka
