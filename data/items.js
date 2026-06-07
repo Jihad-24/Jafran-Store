a52ebfeb322db7ec465f9f37be9d6508e6152679
Jihad-24 <mdjihat0021@gmail.com>
2026-06-07 19:48:27 +0600

    added item data

--- data/items.js
+++ data/items.js
@@ -65,6 +65,19 @@ export const itemsData = [
     image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
   },
   {
+    title: "Watch",
+    category: "Accessories",
+    price: 200,
+    oldPrice: 250,
+    discount: 20,
+    rating: 4.6,
+    ratingCount: 98,
+    delivery: "Free Delivery",
+    description:
+      "Elegant and modern wristwatch with a sleek design, suitable for both casual and formal wear.",
+    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
+  },
+  {
     title: "Headphones",
     category: "Electronics",
     price: 300,
